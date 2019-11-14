6bf634da9d318597e0ac42a323844fe766dd5462
panji-sn <panji010595@gmail.com>
2019-11-14 12:59:27 +0700

    update kanban

---
+++ client/src/components/CreateTask.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container clickable card">
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" v-if="show">
+      <b-form-group
+        label="Title"
+      >
+        <b-form-input
+          v-model="form.title"
+          type="text"
+          required
+          placeholder="Task Title"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Description">
+        <b-form-input
+          v-model="form.description"
+          required
+          placeholder="Task Short Description"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Point">
+        <b-form-input
+          v-model="form.point"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Assigned To">
+        <b-form-input
+          v-model="form.assigned"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        title: '',
+        description: '',
+        point: 0,
+        assigned: '',
+        status: ''
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      this.form.status = 'ToDo'
+      console.log(this.form)
+      this.$emit('Data', this.form)
+    },
+    onReset (evt) {
+      // Reset our form values
+      this.form.title = ''
+      this.form.description = ''
+      this.form.point = 0
+      this.form.assigned = ''
+      this.form.status = ''
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <NavBar @Data='submitdata'></NavBar>
+    <TaskList></TaskList>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+import db from '../config/firestore'
+import TaskList from '@/components/TaskList.vue'
+export default {
+  name: 'home',
+  components: {
+    NavBar,
+    TaskList
+  },
+  methods: {
+    submitdata: (data) => {
+      console.log(data)
+      db.collection('lists')
+        .add(data)
+        .then((result) => {
+          console.log('sukses')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+  data: () => {
+    return {
+      // submitData: ''
+    }
+  }
+}
+</script>
+<style>
+#home {
+  background-color: aqua;
+  }
+</style>
