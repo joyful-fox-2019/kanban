07c128c546b1f5bdf8c88ebf98b7cd1f9d2f3886
dadepe <rizky_dwifutra@yahoo.com>
2019-11-15 20:52:27 +0700

    Kanban Rizky DP

---
+++ client/config/firebase.js
@@ -0,0 +1,13 @@
+const firebase = require("firebase/app");
+// Required for side-effects
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID
+  });
+
+module.exports = firebase
+  
---
+++ client/src/components/CategoryComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="card-deck mb-3 text-center">
+      <div class="card mb-4 shadow-sm">
+        <div :style="{backgroundColor:warna}" class="card-header">
+          <h4 class="my-0 font-weight-normal">{{category}}</h4>
+        </div>
+          <TaskComponent
+          v-for="taskItem in dataParent" :key="taskItem.id"
+          :taskItem="taskItem"
+          @taskDetail="taskDetail"
+          />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskComponent from '../components/TaskComponent'
+export default {
+  props: ['all', 'category', 'dataParent', 'warna'],
+  components: {
+    TaskComponent
+  },
+  methods :{
+    taskDetail(payload){
+      this.$emit('taskDetail', payload)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
