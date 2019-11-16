39c50027f88afbc1669e707baafc3a1f0c0fc984
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-16 23:34:18 +0700

    move kanban components to folder. add footer. add task function done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,18 @@
 <template>
-  <nav class="navbar is-fixed-top is-info" role="navigation" aria-label="main navigation">
+  <nav
+    class="navbar is-fixed-top is-info"
+    role="navigation"
+    aria-label="main navigation"
+  >
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <router-link class="navbar-item" to="/">
         <img src="../assets/logo.png" width="112" height="28" />
-      </a>
+      </router-link>
 
       <a
         role="button"
         class="navbar-burger burger"
-        :class="{'is-active': burgerToggle}"
+        :class="{ 'is-active': burgerToggle }"
         aria-label="menu"
         aria-expanded="false"
         data-target="kanbanNavbar"
@@ -21,10 +25,18 @@
       </a>
     </div>
 
-    <div id="kanbanNavbar" class="navbar-menu" :class="{'is-active': burgerToggle}">
+    <div
+      id="kanbanNavbar"
+      class="navbar-menu"
+      :class="{ 'is-active': burgerToggle }"
+    >
       <div class="navbar-start">
-        <router-link class="navbar-item has-text-primary" tag="a" to="/kanban">Kanban</router-link>
-        <router-link class="navbar-item has-text-primary" tag="a" to="/about">About Creator</router-link>
+        <router-link class="navbar-item has-text-primary" tag="a" to="/kanban"
+          >Kanban</router-link
+        >
+        <router-link class="navbar-item has-text-primary" tag="a" to="/about"
+          >About Creator</router-link
+        >
       </div>
 
       <div class="navbar-end">
@@ -33,7 +45,9 @@
             <router-link class="button is-primary" tag="a" to="/signup">
               <strong>Sign Up</strong>
             </router-link>
-            <router-link class="button is-light" tag="a" to="/signin">Sign In</router-link>
+            <router-link class="button is-light" tag="a" to="/signin"
+              >Sign In</router-link
+            >
           </div>
         </div>
       </div>
@@ -51,5 +65,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
